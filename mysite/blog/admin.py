d886eb69c269e93ee6f20cc70cd5420ee73fc350
je1113 <jje320594@gmail.com>
2026-02-09 18:58:50 +0900

    model.py post 모델 생성 및 admin.py에 모델 등록, 0001

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']  # 목록에 표시할 컬럼
+    list_filter = ['created_at', 'author']             # 필터 사이드바
+    search_fields = ['title', 'content']               # 검색 가능 필드
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)           # 제목
+    content = models.TextField()                        # 본문
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # 작성자
+    created_at = models.DateTimeField(auto_now_add=True)  # 작성일 (자동)
+    updated_at = models.DateTimeField(auto_now=True)      # 수정일 (자동)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']  # 최신 글이 먼저
