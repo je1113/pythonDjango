e994a462d2be295a30e7f09c12fb86fd014f17e0
je1113 <jje320594@gmail.com>
2026-02-09 20:38:03 +0900

    Form & CRUD 구현

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),                # /blog/
-    path('<int:post_id>/', views.detail, name='detail'), # /blog/1/
-    path('about/', views.about, name='about'),           # /blog/about/
+    path('', views.index, name='index'),                       # /blog/
+    path('create/', views.create, name='create'),              # /blog/create/
+    path('<int:post_id>/', views.detail, name='detail'),       # /blog/1/
+    path('<int:post_id>/update/', views.update, name='update'),  # /blog/1/update/
+    path('<int:post_id>/delete/', views.delete, name='delete'),  # /blog/1/delete/
+    path('about/', views.about, name='about'),                 # /blog/about/
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
@@ -10,6 +11,39 @@ def index(request):
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog/detail.html', {'post': post})
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # DB 저장 전 객체만 생성
+            post.author = request.user       # 현재 로그인 사용자를 작성자로
+            post.save()
+            return redirect('detail', post_id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'blog/form.html', {'form': form, 'title': '새 글 작성'})
+
+
+def update(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)  # 기존 데이터에 덮어쓰기
+        if form.is_valid():
+            form.save()
+            return redirect('detail', post_id=post.id)
+    else:
+        form = PostForm(instance=post)  # 기존 데이터를 폼에 채워서 보여줌
+    return render(request, 'blog/form.html', {'form': form, 'title': '글 수정'})
+
+
+def delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('index')
+    return render(request, 'blog/delete.html', {'post': post})
 
 
 def about(request):
